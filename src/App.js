2df3e8116f707ba82a5bd196ccc9ca7c09e8b510
Rohit Chakraborty <rohchakr@gmail.com>
2021-01-24 11:38:47 +0530

    Hello World!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => (
+  <div>
+    Hello from Rohit!
+  </div>
+)
 
 export default App;
